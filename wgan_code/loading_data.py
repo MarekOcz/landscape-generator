bb330e040751f5d98e5b485e181db85277550219
MarekOcz <kitty29@talktalk.net>
2023-09-04 08:07:15 +0100

    Documented the load_dataset function in loading_data.py.

--- wgan_code/loading_data.py
+++ wgan_code/loading_data.py
@@ -2,13 +2,23 @@ import math
 from torchvision.datasets import ImageFolder
 from torchvision.transforms import ToTensor, Compose, Normalize
 from torch.utils.data import DataLoader
-from torch.cuda import is_available
-from tqdm.auto import tqdm
 from pathlib import Path
 
 
-def load_dataset(folder: str | Path, batch_size: int = 64, img_channels: int = 3, device='cpu', num_workers: int = 0,
-                 pin_memory: bool = False):
+def load_dataset(folder: str | Path, batch_size: int = 64, img_channels: int = 3, device: str = 'cpu',
+                 num_workers: int = 0, pin_memory: bool = False) -> DataLoader:
+    """
+    Loads the dataset with normalised images of 0.5 mean and standard deviation as tensors
+    :param folder:  The folder where the data is stored. Ensure the data is inside another folder to mimic the labels
+    ImageFolder class to work e.g. root/dataset/images/1.png, 2.png, ...
+    :param batch_size: The batch size of the loaded data
+    :param img_channels: The number of image channels
+    :param device: The device to load the data onto. Must be a string and not a torch.device object
+    :param num_workers: The number of cores to load the data onto
+    :param pin_memory: If True, the data loader will copy Tensors into device/CUDA pinned memory before returning them.
+    (copied from torch.utils.data.DataLoader docs).
+    :return: An iterable dataloader containing all the images
+    """
     transforms = Compose([
         ToTensor(),
         Normalize([0.5] * img_channels,
